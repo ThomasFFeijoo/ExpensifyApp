d4087041f387ddafa6d4855326974c1bfe4529cf
Thomas <thomwe8@hotmail.com>
2018-04-22 18:58:16 -0300

    reducers and dummy data

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -23,7 +23,12 @@ const setCount = ({ count }) => ({
     count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -44,7 +49,9 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
